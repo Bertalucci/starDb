98f4f872ef05e812b3f880231af656616b17b797
Bertalucci <bertamalfitano@gmail.com>
2022-07-19 15:31:26 +0300

    Add RandomPlanet

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../header/header';
+import RandomPlanet from "../random-planet/random-planet";
 
 const App = () => {
     return (
         <div>
             <Header />
+            <RandomPlanet />
         </div>
     );
 };
