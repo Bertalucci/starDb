abab541f58fe68705b34cf2ace229cfe70eb3ec0
Bertalucci <bertamalfitano@gmail.com>
2022-07-19 18:30:56 +0300

    Add ItemList & PersonDetails

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 import Header from '../header/header';
 import RandomPlanet from "../random-planet/random-planet";
+import ItemList from "../item-list/item-list";
+import PersonDetails from "../person-details/person-details";
 
 const App = () => {
     return (
         <div>
             <Header />
             <RandomPlanet />
+
+            <div className="row mb2">
+                <div className="col md6">
+                    <ItemList />
+                </div>
+                <div className="col md6">
+                    <PersonDetails />
+                </div>
+            </div>
         </div>
     );
 };
